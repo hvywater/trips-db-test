c155d90a52886d98c52c43f4fc063ea8de9d6ecd
hvywater <tangiersintrigue@gmail.com>
2022-04-29 16:42:55 +0100

    New url state, useEffet dependencies

--- src/components/TripList/TripList.js
+++ src/components/TripList/TripList.js
@@ -1,31 +1,40 @@
 import { useEffect, useState } from "react";
 
-import "./TripList.css"
+import "./TripList.css";
 
 const TripList = () => {
   // eslint-disable-next-line no-unused-vars
   const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
-    fetch("http://localhost:3000/trips")
+    fetch(url)
       .then((res) => res.json())
       .then((json) => setTrips(json));
-  }, []);
-
-  console.log(trips);
+  }, [url]);
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
       <ul>
         {trips.map((trip) => (
-          <li id={trips.id}>
+          <li key={trip.id}>
             <h3>{trip.title}</h3>
             <p>{trip.price}</p>
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 };
